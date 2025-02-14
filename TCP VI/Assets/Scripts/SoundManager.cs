8da67ab6fd2abf47bcb6520f05c48c1ef7baab53
Leonardo Barros <leonardo.alves.barros.1@gmail.com>
2025-02-14 03:14:32 -0300

    v0.12.30 - Bugs na CombatScene consertados

--- TCP VI/Assets/Scripts/SoundManager.cs
+++ TCP VI/Assets/Scripts/SoundManager.cs
@@ -57,7 +57,4 @@ public class SoundManager : MonoBehaviour
         }
         //Adicione novas musicas aqui com nome da cena
     }
-
-
-
 }
--- TCP VI/Assets/Scripts/UI/CombateManager.cs
+++ TCP VI/Assets/Scripts/UI/CombateManager.cs
@@ -34,7 +34,7 @@ public class CombateManager : MonoBehaviour
             StartCoroutine(Contagem());
         }
     }
-    
+
 
     IEnumerator Contagem()
     {
