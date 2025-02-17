8c41ff665c255b7ff38a7c96fa876ea5fa772bbe
Leonardo Barros <leonardo.alves.barros.1@gmail.com>
2025-02-17 06:28:06 -0300

    Revert "Reapply "v0.13.6 - Tela de Pause implementada + ajustes""
    This reverts commit 8ad14e9709cd5b00e14c96640cbfa4dd1e690d17.

--- TCP VI/Assets/Scripts/UI/CombateManager.cs
+++ TCP VI/Assets/Scripts/UI/CombateManager.cs
@@ -1,8 +1,9 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
-
 public class CombateManager : MonoBehaviour
 {
     private bool inicioCombat;
@@ -11,41 +12,38 @@ public class CombateManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI textoTempo;
     [SerializeField] float tempo;
 
-    [SerializeField] GameObject telaDePause;
-    private bool isPaused = false;
+    // private Combatant combatant;
 
     void Start()
     {
         Time.timeScale = 0.0f;
         inicioCombat = true;
-
+        
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
-        telaDePause.SetActive(false);
     }
+
 
     void Update()
     {
-        if (inicioCombat && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)))
+        if (inicioCombat && Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) && inicioCombat)
         {
             inicioCombat = false;
             controleUI.SetActive(false);
             foreach (GameObject u in uis)
             {
-                if (u != null)
-                    u.SetActive(true);
+                if(u!= null)
+                u.SetActive(true);
             }
 
             StartCoroutine(Contagem());
         }
-
-        // Chama a função de menu de pausa
-        CheckPauseInput();
     }
+
 
     IEnumerator Contagem()
     {
+
         textoTempo.gameObject.SetActive(true);
         textoTempo.text = "3";
         yield return new WaitForSecondsRealtime(tempo);
@@ -57,71 +55,6 @@ public class CombateManager : MonoBehaviour
         yield return new WaitForSecondsRealtime(tempo);
         textoTempo.gameObject.SetActive(false);
         Time.timeScale = 1.0f;
-    }
 
-    // MENU DE PAUSA
-    public void CheckPauseInput()
-    {
-        // Verifica se o jogador pressionou o ESC para alternar pausa/despausa
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            TogglePause();
-        }
-    }
-
-    void TogglePause()
-    {
-        if (!isPaused)
-        {
-            PauseGame();
-        }
-        else
-        {
-            ResumeGame();
-        }
-    }
-
-    public void PauseGame()
-    {
-        // Pausa o jogo
-        isPaused = true;
-        Time.timeScale = 0f;  // Congela o tempo
-
-        telaDePause.SetActive(true);  // Exibe o menu de pausa
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-    }
-
-    public void ResumeGame()
-    {
-        // Retoma o jogo
-        isPaused = false;
-        Time.timeScale = 1f;  // Volta o tempo ao normal
-
-        ClosePauseMenu();
-    }
-
-    public void ClosePauseMenu()
-    {
-        telaDePause.SetActive(false);  // Esconde o menu de pausa
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    // Troca de cenas
-    public void GoToMainMenu()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
-
-    public void GoToCustomizationScene()
-    {
-        SceneManager.LoadScene("CustomizationScene");
-    }
-
-    // Fechar o jogo
-    public void CloseGame()
-    {
-        Application.Quit();
     }
 }
